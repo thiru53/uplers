d5bc66934153fa11bc47318709207d6d82caa941
Tiru <thiru53@gmail.com>
2023-06-26 22:00:41 +0530

    FindPossibleSubArraysV1 is just finding all possible sub Arrays using basic core java loops

--- javas/FindPossibleSubArraysV1.java
+++ javas/FindPossibleSubArraysV1.java
@@ -1,7 +1,10 @@
+package javas;
+
 public class FindPossibleSubArraysV1 {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //System.out.println("Welcome");
 
         int[] inputArr = {1, 2, 3, 4};
         for(int i=0; i< inputArr.length; i++){
