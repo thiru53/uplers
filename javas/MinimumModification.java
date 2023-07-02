55e03579a34cc1e9005d9804783e69dd66b7780a
Tiru <thiru53@gmail.com>
2023-07-02 21:31:17 +0530

    Minimum Modification

---
+++ javas/MinimumModification.java
@@ -0,0 +1,19 @@
+package javas;
+
+/**
+ * Develop a function that takes an array of positive integers representing the values of coins in your possession. The function should determine the minimum amount of change, defined as the smallest sum of money that cannot be created using the given coins. It is possible to have multiple coins of the same value, and the coins can have any positive integer value.
+ *
+ * For instance, if the array of coins is [1, 2, 5], the minimum amount of change that cannot be created is 4. In the case of an empty coin array, the minimum amount of change that cannot be created is 1.
+ *
+ * Sample Input
+ * coins = [5, 7, 1, 1, 2, 3, 22]
+ *
+ * Sample Output
+ * 20
+ *
+ */
+public class MinimumModification {
+    public static void main(String[] args) {
+
+    }
+}
