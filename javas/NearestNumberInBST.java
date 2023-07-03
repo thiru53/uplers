31f709d9939a780f3ade661874f9807f5c7ab328
Tiru <thiru53@gmail.com>
2023-07-03 11:25:59 +0530

    Nearest Number In BST

--- javas/NearestNumberInBST.java
+++ javas/NearestNumberInBST.java
@@ -1,5 +1,7 @@
 package javas;
 
+import java.util.Objects;
+
 /**
  * Implement a function that takes a Binary Search Tree (BST) and a target integer value as inputs. The function should find and return the value in the BST that is closest to the given target value. You can assume that there will be only one closest value.
  *
@@ -22,5 +24,35 @@ public class NearestNumberInBST {
 
     public static void main(String[] args) {
 
+    }
+
+    public static int findClosestValueInBstUsingLoops(BST tree, int target) {
+        // Write your code here.
+        BST minNode =  tree;
+        while(Objects.nonNull(tree)){
+
+            if(Math.abs(tree.value - target) < Math.abs(minNode.value - target)) {
+                minNode = tree;
+            }
+            if(tree.value == target){
+                minNode = tree;
+                break;
+            } else if(tree.value > target) {
+                tree = tree.left;
+            } else {
+                tree = tree.right;
+            }
+        }
+        return minNode.value;
+    }
+    //Don't change this
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
     }
 }
