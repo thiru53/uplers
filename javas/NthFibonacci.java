0663eef02111e3fbcfc0c5e2a4bed764f1db9303
Tiru <thiru53@gmail.com>
2023-07-23 09:52:54 +0530

    NthFibinacci

---
+++ javas/NthFibonacci.java
@@ -0,0 +1,27 @@
+package javas;
+
+public class NthFibonacci {
+    public static void main(String[] args) {
+
+        int index = 15;
+        int res = getNthFibonacciUsingLoops(index);
+        System.out.println("Test for "+index+", Res : "+res);
+    }
+
+    public static int getNthFibonacciUsingLoops(int n) {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        if(n < 3){
+            return n-1;
+        }
+        for(int i=3; i<=n; i++){
+            c = a+b;
+            a=b;
+            b=c;
+            //System.out.println(i+"-"+c);
+        }
+        return c;
+    }
+}
